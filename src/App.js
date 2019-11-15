528b2a3ecc9cd7ce71d0069f2f8b62613f9c35e5
Marceloni Lima <marcelonipromucena@gmail.com>
2019-11-15 10:42:35 -0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Provider } from "react-redux";
+
+import store from "./store";
+
+import Cart from "./Cart";
+
+const App = () => (
+  <Provider store={store}>
+    <Cart/>
+  </Provider>
+);
+
+export default App;
